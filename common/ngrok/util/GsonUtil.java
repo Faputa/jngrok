d7ad1a882d297719b91834e639db061d2f4c1326
kinocastle <2238932729@qq.com>
2018-06-07 09:54:17 +0800

    优化代码结构

--- common/ngrok/util/GsonUtil.java
+++ common/ngrok/util/GsonUtil.java
@@ -7,12 +7,12 @@ public class GsonUtil
 	}
 
 	private static com.google.gson.GsonBuilder builder = new com.google.gson.GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss");
-	
+
 	public static String toJson(Object object)
 	{
 		return builder.create().toJson(object);
 	}
-	
+
 	public static <T> T toBean(String json, Class<T> classOfT)
 	{
 		return builder.create().fromJson(json, classOfT);
