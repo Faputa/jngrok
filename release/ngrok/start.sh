c74a8b65c813987f6cdb52110e082a9b2310295a
basecell <2238932729@qq.com>
2020-09-14 15:29:13 +0800

    更新脚本

--- release/ngrok/start.sh
+++ release/ngrok/start.sh
@@ -1,2 +1,2 @@
 DIR=$(dirname $0)
-nohup java -Xbootclasspath/a:$DIR -jar $DIR/ngrok.jar &
+nohup java -Xbootclasspath/a:$DIR -jar $DIR/ngrok.jar >/dev/null 2>&1 &
--- release/ngrokd/start.sh
+++ release/ngrokd/start.sh
@@ -1,2 +1,2 @@
 DIR=$(dirname $0)
-nohup java -Xbootclasspath/a:$DIR -jar $DIR/ngrokd.jar &
+nohup java -Xbootclasspath/a:$DIR -jar $DIR/ngrokd.jar >/dev/null 2>&1 &
